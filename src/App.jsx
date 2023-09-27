2c67fd6cffefbd6c9072ee5d9e2974f56b44262f
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-09-27 15:41:15 -0300

    feat: initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Product} from "./pages/Product.jsx";
+import {Homepage} from "./pages/Homepage.jsx";
+import {Pricing} from "./pages/Pricing.jsx";
+import {PageNotFound} from "./pages/PageNotFound.jsx";
+
 export default function App() {
 
   return (
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/product" element={<Product/>}/>
+        <Route path="/pricing" element={<Pricing/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export function PageNotFound() {
+  return (
+    <div>
+      Page not found! :(
+    </div>
+  );
+}
